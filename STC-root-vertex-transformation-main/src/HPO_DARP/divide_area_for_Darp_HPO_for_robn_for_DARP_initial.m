624d53ae2cedf58580866613da77609f2e2a324c
longzhibaby <57171725+longzhibaby@users.noreply.github.com>
2026-08-10 23:47:02 +0800

    Update divide_area_for_Darp_HPO_for_robn_for_DARP_initial.m

--- STC-root-vertex-transformation-main/src/HPO_DARP/divide_area_for_Darp_HPO_for_robn_for_DARP_initial.m
+++ STC-root-vertex-transformation-main/src/HPO_DARP/divide_area_for_Darp_HPO_for_robn_for_DARP_initial.m
@@ -186,7 +186,7 @@ while stop==0
     end
     for k=1:robot_num
        if dm(k) > threshold || dm(k) < -threshold
-          m(k) = m(k)+0.00001*dm(k);
+          m(k) = m(k)+0.005*dm(k);
        else
            count = count + 1;
        end
